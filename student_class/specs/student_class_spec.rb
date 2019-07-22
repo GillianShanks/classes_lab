dc11671fc42b1e52fe0acad0e9a8be09800a466e
Gillian Shanks <GillianShanks88@gmail.com>
2019-07-22 14:17:58 +0100

    Student class created, and .name tested

---
+++ student_class/specs/student_class_spec.rb
@@ -0,0 +1,15 @@
+require("Minitest/autorun")
+require("Minitest/rg")
+require_relative("../student_class.rb")
+
+class TestStudentClass < MiniTest::Test
+
+  def setup()
+    @student1 = Student.new("Gillian", "G14")
+  end
+
+  def test_get_student_name()
+    assert_equal("Gillian", @student1.name)
+  end
+
+end
---
+++ student_class/student_class.rb
@@ -0,0 +1,12 @@
+class Student
+  def initialize(name, cohort)
+    @name = name
+    @cohort = cohort
+  end
+
+  #getter
+
+  def name()
+    return @name
+  end
+end
